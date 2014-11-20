80450876d6e08d9dd2c57603105eb66fda669f4c
Roman Frołow <rofrol@gmail.com>
2014-11-20 13:31:37 +0100

    try with resources

--- src/stream/Generate.java
+++ src/stream/Generate.java
@@ -6,29 +6,29 @@ import java.io.OutputStream;
 public class Generate {
 
 	public static void main(String[] args) {
-		generateCharacters(System.out);
+		try (OutputStream out = System.out) {
+			generateCharacters(System.out);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
-	static void generateCharacters(OutputStream out) {
+	static void generateCharacters(OutputStream out) throws IOException {
 		int firstPrintable = 33;
 		int offset = 0;
 		int lineWidth = 72;
 		int range = 94;
 		byte[] line = new byte[lineWidth + 2];
-		try {
-			for(int r = 0; r < 30; r++) {
-				for(int i = 0; i < lineWidth; i++) {
-					line[i] = (byte)(firstPrintable + offset + i);
-				}
-				offset = (offset + 1) % range;
-				line[72] = (byte)'\r';
-				line[73] = (byte)'\n';
-				out.write(line);
+		for(int r = 0; r < 30; r++) {
+			for(int i = 0; i < lineWidth; i++) {
+				line[i] = (byte)(firstPrintable + offset + i);
 			}
-			out.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
+			offset = (offset + 1) % range;
+			line[72] = (byte)'\r';
+			line[73] = (byte)'\n';
+			out.write(line);
 		}
+		out.flush();
 	}
 }
